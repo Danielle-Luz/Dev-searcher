7f659e19a667d514b305d7865d7ef03614e50aa5
Axionauta <daniln2209@gmail.com>
2022-12-02 17:52:55 -0300

    feat: add error message when request limit is reached

--- scripts/api.js
+++ scripts/api.js
@@ -4,29 +4,37 @@ async function getRequest (url) {
         const response = await fetch(url);
         const responseJson = await response.json();
 
-        userWasFound(responseJson);
-
         removeNotFoundMessage();
 
-        return responseJson;
+        const wasFound = userWasFound(responseJson);
+        
+        return wasFound ? responseJson : undefined;
     } catch (err) {
         showNotFoundMessage();
     }
 }
 
 function userWasFound ({message}) {
-    if (message == "Not Found") {
-        throw new Exception();
+    if (message) {
+        if (message == "Not Found") {
+            showNotFoundMessage("Usuário não encontrado.");
+        } else {
+            showNotFoundMessage("Limite de requisições diário atingido.");
+        }
+        return false;
     }
+
+    return true;
 }
 
-function showNotFoundMessage () {
+function showNotFoundMessage (message) {
     const notFoundElement = document.querySelector(".not-found-message");
     
     if (!notFoundElement) {
         const inputWrapper = document.querySelector(".input-wrapper");
-        const notFoundMessage = '<span class="not-found-message text-2">Usuário não encontrado</span>';
-    
+        const notFoundMessage = `<span class="not-found-message text-2">${message}</span>`;
+        
+        console.log(notFoundMessage)
         inputWrapper.insertAdjacentHTML("beforeend", notFoundMessage);
     }
 }
